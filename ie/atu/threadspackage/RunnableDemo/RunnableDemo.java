b101047be8026d156b0abd1fa04a62a1f675faf5
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-10-21 12:39:02 +0000

    updates

--- ie/atu/threadspackage/RunnableDemo/RunnableDemo.java
+++ ie/atu/threadspackage/RunnableDemo/RunnableDemo.java
@@ -3,22 +3,36 @@ package ie.atu.threadspackage.RunnableDemo;
 class RunnableDemo implements Runnable {
 
 	public void run() {
-		// When a thread is created, it is given a name and a priority.
-		System.out.println(Thread.currentThread().getName() +" is running with priority " + Thread.currentThread().getPriority());
-		
+		// Print the Name, Priority, State amd Alive status of the thread.
+		System.out.println("Name: " + Thread.currentThread().getName() + 
+						   ", Priority: " + Thread.currentThread().getPriority() +
+						   ", State: " + Thread.currentThread().getState() +
+						   ", Alive: " + Thread.currentThread().isAlive());
+
 		// This is the code that will be executed by the thread
 		for (int i = 10; i > 0; i--) {
-			// Print the output of the thread. Show its name and the value of i as it counts down.
+
+			/*
+			 * Print the output of the thread. Show the thread name and the value of i as it
+			 * counts
+			 * down from 10 to 1.
+			 */
 			System.out.println(Thread.currentThread().getName() + " countdown: " + i);
+			/*
+			 * Let the thread sleep for 1 millisecond between each countdown so that it
+			 * does not finish executing before the other thread has a chance to start. We
+			 * need to surround the sleep in a try/catch block because the sleep method can
+			 * throw an InterruptedException.
+			 */
+
 			try {
-				// Let the thread sleep for 10 milliseconds between each countdown so that it does not run too fast.
-				Thread.sleep(10); 
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				System.out.println(e);
 			}
-		}
+		} // end for loop
 
-		System.out.println("Thread exiting");
-	}
+		System.out.println(Thread.currentThread().getName() + " exiting run method");
 
-}
+	} // end run method	
+} // End of class RunnableDemo
--- ie/atu/threadspackage/RunnableDemo/RunnableDemoDriver.java
+++ ie/atu/threadspackage/RunnableDemo/RunnableDemoDriver.java
@@ -2,13 +2,33 @@ package ie.atu.threadspackage.RunnableDemo;
 
 public class RunnableDemoDriver {
 	public static void main(String args[]) {
+
+		// Variables to store thread states
+		String thread1State = null;
+		String thread2State = null;
+
 		RunnableDemo runnableObject = new RunnableDemo();
-		// Allocate two new thread objects and pass the runnable object to them and give them names.
+		// Allocate two new threads. Pass them the runnable object and pass them a name.
 		Thread thread1 = new Thread(runnableObject, "Thread 1");
 		Thread thread2 = new Thread(runnableObject, "Thread 2");
+
 		// Start the threads
 		thread1.start();
 		thread2.start();
-	}
 
-}
+		// Monitor for changes in thread state and print them out.
+		while (thread1.isAlive() || thread2.isAlive()) {
+			// If the state of Thread 1 changes then print it to the console
+			if (thread1State != thread1.getState().toString()) {
+				thread1State = thread1.getState().toString();
+				System.out.println(thread1.getName() + " state: " + thread1State);
+			}
+			// If the state of Thread 2 changes then print it to the console
+			if (thread2State != thread2.getState().toString()) {
+				thread2State = thread2.getState().toString();
+				System.out.println(thread2.getName() + " state: " + thread2State);
+			}
+		} // end while loop
+
+	} // End of main method
+} // End of class RunnableDemoDriver
