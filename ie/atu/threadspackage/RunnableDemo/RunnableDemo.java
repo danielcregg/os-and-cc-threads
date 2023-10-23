fe44d3362d6534559807da4267b9cebf4e71d674
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-23 11:19:07 +0000

    Clean up and add commentary

--- ie/atu/threadspackage/RunnableDemo/RunnableDemo.java
+++ ie/atu/threadspackage/RunnableDemo/RunnableDemo.java
@@ -3,20 +3,10 @@ package ie.atu.threadspackage.RunnableDemo;
 class RunnableDemo implements Runnable {
 
 	public void run() {
-		// Print the Name, Priority, State amd Alive status of the thread.
-		System.out.println("Name: " + Thread.currentThread().getName() + 
-						   ", Priority: " + Thread.currentThread().getPriority() +
-						   ", State: " + Thread.currentThread().getState() +
-						   ", Alive: " + Thread.currentThread().isAlive());
 
-		// This is the code that will be executed by the thread
+		// This is the code that will be executed by the thread. It will count down from 10 to 1.
 		for (int i = 10; i > 0; i--) {
-
-			/*
-			 * Print the output of the thread. Show the thread name and the value of i as it
-			 * counts
-			 * down from 10 to 1.
-			 */
+			// Print thread name and the value of i as it counts down from 10 to 1.
 			System.out.println(Thread.currentThread().getName() + " countdown: " + i);
 			/*
 			 * Let the thread sleep for 1 millisecond between each countdown so that it
@@ -24,15 +14,12 @@ class RunnableDemo implements Runnable {
 			 * need to surround the sleep in a try/catch block because the sleep method can
 			 * throw an InterruptedException.
 			 */
-
 			try {
+				// Let thread sleep for 1 millisecond
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				System.out.println(e);
 			}
 		} // end for loop
-
-		System.out.println(Thread.currentThread().getName() + " exiting run method");
-
-	} // end run method	
+	} // end run method
 } // End of class RunnableDemo
--- ie/atu/threadspackage/RunnableDemo/RunnableDemoDriver.java
+++ ie/atu/threadspackage/RunnableDemo/RunnableDemoDriver.java
@@ -1,38 +1,63 @@
 package ie.atu.threadspackage.RunnableDemo;
 
+/* This program demonstrates how to create a thread by implementing the Runnable interface.
+ * We crate two threads and pass them the same runnable object which is an instance of the
+ * RunnableDemo class. We then start the threads and monitor their state. We also print out
+ * the name of the thread, its priority, its state and whether or not it is alive. 
+ */
 public class RunnableDemoDriver {
 	public static void main(String args[]) {
 
-		// Variables to store thread states
+		// Create to string variables to store the state of each thread
 		String thread1State = "unknown";
 		String thread2State = "unknown";
 
 		RunnableDemo runnableObject = new RunnableDemo();
 		// Allocate two new threads. Pass them the runnable object and pass them a name.
 		Thread thread1 = new Thread(runnableObject, "Thread 1");
+		// When thread1 is created it is in the NEW state and its Alive status is false.
+		printThreadInfo(thread1);
 		Thread thread2 = new Thread(runnableObject, "Thread 2");
+		// When thread2 is created it is in the NEW state and its Alive status is false.
+		printThreadInfo(thread2);
 
-		// Start the threads
+		// Start the threads i.e. change state from NEW to RUNNABLE and 
+		// Alive status to true
 		thread1.start();
+		printThreadInfo(thread1);
 		thread2.start();
+		printThreadInfo(thread2);
 
-		// Monitor for changes in thread state and print them out.
+		// While thread1 OR thread2 is alive
 		while (thread1.isAlive() || thread2.isAlive()) {
-			// If the state of Thread 1 is NOT equal to the currently stored state
+			// Check if the state of thread1 is NOT equal to the currently stored state
 			if (!thread1State.equals(thread1.getState().toString())) {
-				// Update the stored state
+				// If it is not then update the stored state
 				thread1State = thread1.getState().toString();
-				// Print the new state of Thread 1 to the console
-				System.out.println(thread1.getName() + " state: " + thread1State);
-			}
-			// If the state of Thread 2 is NOT equal to the currently stored state
+				// Print the new state of thread1 to the console
+				System.out.println("***" + thread1.getName() + " state: " + thread1State + "***");
+			} // end if
+				// Check if the state of thread2 is NOT equal to the currently stored state
 			if (!thread2State.equals(thread2.getState().toString())) {
-				// Update the stored state
+				// If it is not then update the stored state
 				thread2State = thread2.getState().toString();
-				// Print the new state of Thread 2 to the console
-				System.out.println(thread2.getName() + " state: " + thread2State);
-			}
+				// Print the new state of thread2 to the console
+				System.out.println("***" + thread2.getName() + " state: " + thread2State + "***");
+			} // end if
 		} // end while loop
-
+		
+		// Print the Name, Priority, State and Alive status of the thread after it has
+		// finished executing.
+		printThreadInfo(thread1);
+		printThreadInfo(thread2);
 	} // End of main method
+
+	// Method to print the Name, Priority, State and Alive status of a given thread.
+	static void printThreadInfo(Thread thread) {
+		System.out.println("***Name: " + thread.getName() +
+				", Priority: " + thread.getPriority() +
+				", State: " + thread.getState() +
+				", Alive: " + thread.isAlive() +
+				"***");
+	}
 } // End of class RunnableDemoDriver
